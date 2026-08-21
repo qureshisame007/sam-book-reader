385e4d37ad97fee2e56d3910c663e067d2ff5cde
Sameer Qureshi <41162405+qureshisame007@users.noreply.github.com>
2026-08-21 22:54:40 +0530

    Make icon optional in build script

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,16 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        use std::path::Path;
+        let icon_path = Path::new("../icons/icon.ico");
+        
+        // Only fail if icon doesn't exist
+        if !icon_path.exists() {
+            println!("cargo:warning=Icon file not found at {:?}, using default", icon_path);
+            // Don't panic - allow build to continue
+            return;
+        }
+    }
+    
     tauri_build::build()
 }
